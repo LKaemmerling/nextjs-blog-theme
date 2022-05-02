c08249cb5f219cf88bae366f02370bf35532e0a2
Lukas Kämmerling <github@lukas-kaemmerling.de>
2022-05-02 20:57:27 +0200

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,19 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+        <script>
+    (function(f, a, t, h, o, m){
+      a[h]=a[h]||function(){
+        (a[h].q=a[h].q||[]).push(arguments)
+      };
+      o=f.createElement('script'),
+              m=f.getElementsByTagName('script')[0];
+      o.async=1; o.src=t; o.id='fathom-script';
+      m.parentNode.insertBefore(o,m)
+    })(document, window, '//localhost:8080/tracker.js', 'fathom');
+    fathom('set', 'siteId', 'YTNPA');
+    fathom('trackPageview');
+  </script>
     </Head>
   );
 }
